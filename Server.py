6a774ced0578f2f02fcac895b9d500898cdbba2c
Nilesh Popli <77211167+adrenex@users.noreply.github.com>
2022-05-23 03:07:26 +0530

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,58 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+
+clients = {}
+addresses = {}
+
+HOST = "127.0.0.1" # local ip address or private ip address
+PORT = 5000
+ADDR = (HOST, PORT)   #to make a tuple
+SOCK = socket(AF_INET, SOCK_STREAM) # server side socket
+SOCK.bind(ADDR) # since bind function takes only 1 arguement 
+
+
+def accept_incoming_connections():
+    """Sets up handling for incoming clients."""
+    while True:
+        client, client_address = SOCK.accept()
+        print("%s:%s has connected." % client_address)
+        client.send("Greetings from the ChatRoom! ".encode("utf8"))
+        client.send("Now type your name and press enter!".encode("utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client, client_address)).start()
+
+
+def handle_client(conn, addr):  # Takes client socket as argument.
+    """Handles a single client connection."""
+    name = conn.recv(1024).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type #quit to exit.' % name
+    conn.send(bytes(welcome, "utf8"))
+    msg = "%s from [%s] has joined the chat!" % (name, "{}:{}".format(addr[0], addr[1]))
+    broadcast(bytes(msg, "utf8"))
+    clients[conn] = name
+    while True:
+        msg = conn.recv(1024)
+        if msg != bytes("#quit", "utf8"):
+            broadcast(msg, name + ": ")
+        else:
+            conn.send(bytes("#quit", "utf8"))
+            conn.close()
+            del clients[conn]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    """Broadcasts a message to all the clients."""
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8") + msg)
+
+
+if __name__ == "__main__":
+    SOCK.listen(5)  # Listens for 5 connections at max.
+    print("Chat Server has Started !!")
+    print("Waiting for connections...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()  # Starts the infinite loop.
+    ACCEPT_THREAD.join()
+    SOCK.close()
